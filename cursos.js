76980ca32ac10e77a11a5d0746300c36bae8f564
Anderson Alvarez V <andersonav91@gmail.com>
2019-05-19 14:54:30 -0500

    Tarea semana 1

---
+++ cursos.js
@@ -0,0 +1,8 @@
+let cursos = [
+	{id: 1, nombre: 'Fundamentos Node', duracion: '20 Horas', valor: '$100.000'},
+	{id: 2, nombre: 'Laravel', duracion: '30 Horas', valor: '$300.000'},
+	{id: 3, nombre: 'Cisco 1', duracion: '80 Horas', valor: '$1.00.000'},
+	{id: 4, nombre: 'Xp', duracion: '50 Horas', valor: '$500.000'}
+];
+
+module.exports = cursos;
