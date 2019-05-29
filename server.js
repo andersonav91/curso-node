0784db26cb0becb71b059d7b43627ccad2fd827d
Anderson Alvarez V <andersonav91@gmail.com>
2019-05-29 09:41:40 -0500

    * Configurado el servidor de express. * Configurada la aplicación para soportar plantillas hbs.js * Agregado bootstrap para el ui

--- server.js
+++ server.js
@@ -1,10 +1,39 @@
+var fs = require('fs');
 var express = require('express');
+var hbs = require('hbs');
+var path = require('path');
+
 var app = express();
 
+hbs.registerPartial('menu', fs.readFileSync(__dirname + '/vistas/menu.hbs', 'utf8'));
+
+app.set('view engine', 'hbs');
+
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.locals = {
+        some_value: 'foo bar',
+        list: ['cat', 'dog']
+    }
+    res.render(path.join(__dirname + '/vistas/cursos.hbs'));
 });
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+    // cursos
+    fs.exists("cursos.json", function (exists) {
+        if(! exists)
+        {
+            fs.writeFile("cursos.json", JSON.stringify({}), function (err, data) {});
+        }
+    });
+
+    // usuarios
+    fs.exists("usuarios.json", function (exists) {
+        if(! exists)
+        {
+            fs.writeFile("usuarios.json", JSON.stringify({}), function (err, data) {})
+        }
+    });
 });
+
+
+
