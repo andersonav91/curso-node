a1aabea111e3e574dffc46d1c63f9829a1130c87
Anderson Alvarez V <andersonav91@gmail.com>
2019-05-30 09:39:26 -0500

    * Añadida la opción de guardar cursos, caso de uso 1

--- server.js
+++ server.js
@@ -2,46 +2,70 @@ var fs = require('fs');
 var express = require('express');
 var hbs = require('hbs');
 var path = require('path');
+var bodyParser = require("body-parser");
 
 var app = express();
 
 hbs.registerPartial('menu', fs.readFileSync(__dirname + '/vistas/menu.hbs', 'utf8'));
 
 app.set('view engine', 'hbs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function (req, res) {
-    res.locals = {
-        some_value: 'foo bar',
-        list: ['cat', 'dog']
-    }
     res.render(path.join(__dirname + '/vistas/cursos.hbs'));
 });
 
 app.get('/crear-cursos', function (req, res) {
+    res.render(path.join(__dirname + '/vistas/crear-cursos.hbs'));
+});
+
+app.post('/guardar-cursos', function (req, res) {
+    const cursos = require('./cursos.json');
+    var guardado = false;
+    const body = req.body;
+    var existeCurso = cursos.some( m => m.idCurso === body['idCurso']);
+    var cursosActuales = cursos;
+    cursosActuales.push({
+        idCurso: body['idCurso'],
+        nombre: body['nombre'],
+        descripcion: body['descripcion'],
+        valor: body['valor'],
+        intensidad: body['intensidad'],
+        modalidad: body['modalidad'],
+        estado: 'disponible',
+    });
+    if(! existeCurso){
+        guardado = true;
+        escribirArchivo("cursos.json", cursosActuales);
+    }
     res.locals = {
-        some_value: 'foo bar',
-        list: ['cat', 'dog']
+        existeCurso: existeCurso,
+        guardado: guardado
     }
     res.render(path.join(__dirname + '/vistas/crear-cursos.hbs'));
 });
 
 app.listen(3000, function () {
     // cursos
-    fs.exists("cursos.json", function (exists) {
-        if(! exists)
-        {
-            fs.writeFile("cursos.json", JSON.stringify({}), function (err, data) {});
-        }
-    });
-
-    // usuarios
     fs.exists("usuarios.json", function (exists) {
-        if(! exists)
-        {
-            fs.writeFile("usuarios.json", JSON.stringify({}), function (err, data) {})
+            if (!exists) {
+                // usuarios
+                escribirArchivo("cursos.json", []);
+            }
         }
-    });
+    );
+    fs.exists("usuarios.json", function (exists) {
+            if (!exists) {
+                // usuarios
+                escribirArchivo("usuarios.json", []);
+            }
+        }
+    );
 });
 
+function escribirArchivo(nombre, contenido) {
+    fs.writeFile(nombre, JSON.stringify(contenido), function (err, data) {});
+}
 
 
