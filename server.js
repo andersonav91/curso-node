a45531394a0c25afb50a74f60beefe029137a295
Anderson Alvarez V <andersonav91@gmail.com>
2019-05-30 19:02:27 -0500

    * Terminado el caso de uso 3.

--- server.js
+++ server.js
@@ -42,6 +42,51 @@ app.get('/ver-curso/:id', function (req, res) {
     res.render(path.join(__dirname + '/vistas/ver-curso.hbs'));
 });
 
+app.get('/proceso-inscripcion/', function (req, res) {
+    var cursos = require('./cursos.json');
+    res.locals = {
+        cursos: cursos.filter(function (item) {
+            return item.estado === 'disponible'
+        })
+    };
+    res.render(path.join(__dirname + '/vistas/proceso-inscripcion.hbs'));
+});
+
+app.post('/guardar-proceso-inscripcion/', function (req, res) {
+    var cursos = require('./cursos.json');
+    var inscritos = require('./inscritos.json');
+    var usuarios = require('./usuarios.json');
+    var guardado = false;
+    const body = req.body;
+    var existeUsuarioCurso = inscritos.some(function (item) {
+        return item.idCurso === body.idCurso && item.documento === body.documento
+    });
+    if(! existeUsuarioCurso || existeUsuarioCurso === null){
+        guardado = true;
+        usuarios.push({
+            nombre: body.nombre,
+            correo: body.correo,
+            telefono: body.telefono,
+            documento: body.documento
+        });
+        escribirArchivo("usuarios.json", usuarios);
+        inscritos.push({
+            idCurso: body.idCurso,
+            documento: body.documento
+        });
+        escribirArchivo("inscritos.json", inscritos);
+    }
+    res.locals = {
+        cursos: cursos.filter(function (item) {
+            return item.estado === 'disponible'
+        }),
+        guardado: guardado,
+        existeUsuarioCurso: existeUsuarioCurso
+    };
+    res.render(path.join(__dirname + '/vistas/proceso-inscripcion.hbs'));
+});
+
+
 app.post('/guardar-cursos', function (req, res) {
     const cursos = require('./cursos.json');
     var guardado = false;
@@ -81,6 +126,13 @@ app.listen(3000, function () {
             if (!exists) {
                 // usuarios
                 escribirArchivo("usuarios.json", []);
+            }
+        }
+    );
+    fs.exists("inscritos.json", function (exists) {
+            if (!exists) {
+                // inscritos
+                escribirArchivo("inscritos.json", []);
             }
         }
     );
