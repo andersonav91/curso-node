135176d1c827269b626fc9e83663e19f6a962617
Anderson Alvarez V <andersonav91@gmail.com>
2019-05-30 23:08:05 -0500

    * Terminado el caso de uso 5 y muchas correcciones varias.

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 Curso Node Js - Tecnológico de Antioquia.
 
 # Tarea 2
-Antes de iniciar, correr el comando **npm install express**
+Antes de iniciar, correr el comando **npm install**, esto instalara las dependencias necesarias.
 
 # Cómo correr el servidor ?
 
-node server.js y luego ingresar a http://localhost:3000 
+**npm run** y luego ingresar a **http://localhost:3000** 
 
--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 hbs.registerHelper('obtenerListaInscritos', function(object) {
-    var inscritos = require('./inscritos.json');
-    var usuarios = require('./usuarios.json');
+    var inscritos = JSON.parse(fs.readFileSync('inscritos.json', 'utf8'));
+    var usuarios = JSON.parse(fs.readFileSync('usuarios.json', 'utf8'));
     inscritos = inscritos.filter(function (item) {
         return item.idCurso === object
     });
@@ -32,6 +32,28 @@ hbs.registerHelper('obtenerListaInscritos', function(object) {
   }
 );
 
+hbs.registerHelper('obtenerListaInscritosAEliminar', function(object) {
+    var inscritos = JSON.parse(fs.readFileSync('inscritos.json', 'utf8'));
+    var usuarios = JSON.parse(fs.readFileSync('usuarios.json', 'utf8'));
+    inscritos = inscritos.filter(function (item) {
+        return item.idCurso === object
+    });
+    var ret = "<ul class='list-group list-group-flush'>";
+    inscritos.forEach(function (it) {
+        var usua = usuarios.filter(function (item) {
+            return item.documento === it.documento
+        });
+        usua.forEach(function (i) {
+            ret = ret + "<li class='list-group-item'>" + i.nombre
+                + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +
+                "<a class='btn btn-primary btn-sm' href='/eliminar-inscripcion/" + object + "/" + i.documento + "' role='button'>Eliminar</a>"
+                + "</li>";
+        });
+    });
+    ret = ret + "</ul>";
+    return ret;
+    }
+);
 
 app.get('/', function (req, res) {
     res.render(path.join(__dirname + '/vistas/cursos.hbs'));
@@ -42,7 +64,7 @@ app.get('/crear-cursos', function (req, res) {
 });
 
 app.get('/listar-cursos', function (req, res) {
-    const cursos = require('./cursos.json');
+    var cursos = JSON.parse(fs.readFileSync('cursos.json', 'utf8'));
     res.locals = {
         cursos: cursos.filter(function (item) {
             return item.estado === 'disponible'
@@ -52,17 +74,64 @@ app.get('/listar-cursos', function (req, res) {
 });
 
 app.get('/ver-inscritos', function (req, res) {
-    const cursos = require('./cursos.json');
+    var cursos = JSON.parse(fs.readFileSync('cursos.json', 'utf8'));
+    var inscritos = JSON.parse(fs.readFileSync('inscritos.json', 'utf8'));
+    var usuarios = JSON.parse(fs.readFileSync('usuarios.json', 'utf8'));
     res.locals = {
         cursos: cursos.filter(function (item) {
             return item.estado === 'disponible'
         }),
+        inscritos: inscritos,
+        usuarios: usuarios
     };
     res.render(path.join(__dirname + '/vistas/ver-inscritos.hbs'));
 });
 
+app.get('/eliminar-inscritos', function (req, res) {
+    var cursos = JSON.parse(fs.readFileSync('cursos.json', 'utf8'));
+    var inscritos = JSON.parse(fs.readFileSync('inscritos.json', 'utf8'));
+    var usuarios = JSON.parse(fs.readFileSync('usuarios.json', 'utf8'));
+    res.locals = {
+        cursos: cursos.filter(function (item) {
+            return item.estado === 'disponible'
+        }),
+        inscritos: inscritos,
+        usuarios: usuarios
+    };
+    res.render(path.join(__dirname + '/vistas/eliminar-inscritos.hbs'));
+});
+
+app.get('/eliminar-inscripcion/:idCurso/:documento', function (req, res) {
+    var cursos = JSON.parse(fs.readFileSync('cursos.json', 'utf8'));
+    var inscritos = JSON.parse(fs.readFileSync('inscritos.json', 'utf8'));
+    var usuarios = JSON.parse(fs.readFileSync('usuarios.json', 'utf8'));
+    var otrosUsuarios = inscritos.filter(function (item) {
+        return item.idCurso != req.params.idCurso || item.documento !== req.params.documento
+    });
+    escribirArchivo("inscritos.json", otrosUsuarios);
+    if(! inscritos.some(function (item) {
+        return item.documento === req.params.documento
+    })){
+        otrosUsuarios = usuarios.filter(function (item) {
+            return item.documento !== req.params.documento
+        });
+        escribirArchivo("usuarios.json", otrosUsuarios);
+    }
+
+    res.locals = {
+        cursos: cursos.filter(function (item) {
+            return item.estado === 'disponible'
+        }),
+        inscritos: inscritos,
+        usuarios: usuarios
+    };
+    res.render(path.join(__dirname + '/vistas/eliminar-inscritos.hbs'));
+});
+
 app.post('/cambiar-estado-curso', function (req, res) {
-    var cursos = require('./cursos.json');
+    var cursos = JSON.parse(fs.readFileSync('cursos.json', 'utf8'));
+    var inscritos = JSON.parse(fs.readFileSync('inscritos.json', 'utf8'));
+    var usuarios = JSON.parse(fs.readFileSync('usuarios.json', 'utf8'));
     const body = req.body;
     var idCurso = body.idCurso;
 
@@ -73,13 +142,15 @@ app.post('/cambiar-estado-curso', function (req, res) {
         cursos: cursos.filter(function (item) {
             return item.estado === 'disponible'
         }),
-        cerrado: true
+        cerrado: true,
+        inscritos: inscritos,
+        usuarios: usuarios
     };
     res.render(path.join(__dirname + '/vistas/ver-inscritos.hbs'));
 });
 
 app.get('/ver-curso/:id', function (req, res) {
-    var cursos = require('./cursos.json');
+    var cursos = JSON.parse(fs.readFileSync('cursos.json', 'utf8'));
     const id = req.params.id;
     curso = cursos.filter(function (item) {
         return item.idCurso === id
@@ -91,7 +162,7 @@ app.get('/ver-curso/:id', function (req, res) {
 });
 
 app.get('/proceso-inscripcion/', function (req, res) {
-    var cursos = require('./cursos.json');
+    var cursos = JSON.parse(fs.readFileSync('cursos.json', 'utf8'));
     res.locals = {
         cursos: cursos.filter(function (item) {
             return item.estado === 'disponible'
@@ -101,9 +172,9 @@ app.get('/proceso-inscripcion/', function (req, res) {
 });
 
 app.post('/guardar-proceso-inscripcion/', function (req, res) {
-    var cursos = require('./cursos.json');
-    var inscritos = require('./inscritos.json');
-    var usuarios = require('./usuarios.json');
+    var cursos = JSON.parse(fs.readFileSync('cursos.json', 'utf8'));
+    var inscritos = JSON.parse(fs.readFileSync('inscritos.json', 'utf8'));
+    var usuarios = JSON.parse(fs.readFileSync('usuarios.json', 'utf8'));
     var guardado = false;
     const body = req.body;
     var existeUsuarioCurso = inscritos.some(function (item) {
@@ -111,12 +182,16 @@ app.post('/guardar-proceso-inscripcion/', function (req, res) {
     });
     if(! existeUsuarioCurso || existeUsuarioCurso === null){
         guardado = true;
-        usuarios.push({
-            nombre: body.nombre,
-            correo: body.correo,
-            telefono: body.telefono,
-            documento: body.documento
-        });
+        if(! usuarios.some(function (item) {
+            return item.documento === body.documento
+        })){
+            usuarios.push({
+                nombre: body.nombre,
+                correo: body.correo,
+                telefono: body.telefono,
+                documento: body.documento
+            });
+        }
         escribirArchivo("usuarios.json", usuarios);
         inscritos.push({
             idCurso: body.idCurso,
@@ -136,7 +211,7 @@ app.post('/guardar-proceso-inscripcion/', function (req, res) {
 
 
 app.post('/guardar-cursos', function (req, res) {
-    const cursos = require('./cursos.json');
+    var cursos = JSON.parse(fs.readFileSync('cursos.json', 'utf8'));
     var guardado = false;
     const body = req.body;
     var existeCurso = cursos.some( m => m.idCurso === body['idCurso']);
